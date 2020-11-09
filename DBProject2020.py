07ed9c1f2d16bde12aaa4cb2ac9d71bb93e2b1e7
Naphatsakorn Khotsombat <naphatsakornoshea@gmail.com>
2020-11-09 10:27:44 +0000

    Added a MySQL connector and connected to my DB

--- DBProject2020.py
+++ DBProject2020.py
@@ -1,3 +1,13 @@
-#   access_DB.py
+#   DBProject2020.py
 #   Author:         Naphatsakorn Khotsombat
 #   Description:    This program will be used to access Database from MySQL Database created for plane storage.
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="root"
+)
+
+print(mydb)
