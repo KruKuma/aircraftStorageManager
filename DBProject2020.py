7f08ec62937723189d20834e8f1509f421588206
Naphatsakorn Khotsombat <n.khotsombat@mycit.ie>
2020-11-24 11:52:02 +0000

    Added the menu for managing the aircraft in the database.

--- DBProject2020.py
+++ DBProject2020.py
@@ -5,18 +5,18 @@
 import mysql.connector
 
 
-def initDatabase():
-    mydb = mysql.connector.connect(
+mydb = mysql.connector.connect(
         host="localhost",
         user="root",
-        passwd="root"
+        passwd="root",
+        database="aircraftStorage"
     )
 
-    mycursor = mydb.cursor()
+mycursor = mydb.cursor()
 
-    mycursor.execute("CREATE DATABASE IF NOT EXISTS aircraftStorage")
-    mycursor.execute("CREATE TABLE IF NOT EXISTS aircraft (reg VARCHAR(10) NOT NULL, maker VARCHAR(25), "
-                     "series VARCHAR(25), price INTEGER(20), PRIMARY KEY (reg))")
+mycursor.execute("CREATE DATABASE IF NOT EXISTS aircraftStorage")
+mycursor.execute("CREATE TABLE IF NOT EXISTS aircraft (reg VARCHAR(10) NOT NULL, maker VARCHAR(25), "
+                "series VARCHAR(25), price INTEGER(20), PRIMARY KEY (reg))")
 
 
 def checkForNum(prompt):
@@ -41,7 +41,8 @@ def mainMenu():
     while True:
         userInput = checkForNum("1. Manage the storage system"
                                 "\n2. View Storage"
-                                "\n3. Quit")
+                                "\n3. Quit"
+                                "\n>>")
 
         if 0 < userInput <= 3:
             break
@@ -58,6 +59,26 @@ def mainMenu():
     return userInput
 
 
+def loginOption():
+    isUser = False
+
+    while True:
+        userOption = checkForNum("1. Login - existing account"
+                                 "\n2. Register - new account"
+                                 "\n>>")
+
+        if userOption == 1:
+            isUser = loginMenu()
+
+        elif userOption == 2:
+            userRegMenu()
+
+        else:
+            print("Not an option!")
+
+    return isUser
+
+
 def loginMenu():
     """
     Print out the login screen for the user to input name and password to verified if the name or password
@@ -65,8 +86,11 @@ def loginMenu():
     """
     isUser = False
 
+    print("Welcome to our Aircraft Storage System"
+          "Please enter your user information")
+
     while True:
-        name = input("Name: ")
+        name = input("Username: ")
         password = input("Password: ")
 
         userData = open('loginData.txt', 'r')
@@ -77,31 +101,97 @@ def loginMenu():
         if name == userName and password == userPass:
             print("Welcome " + userName)
             isUser = True
+            break
 
         else:
-            print("Module Record System - Login Failed")
+            print("Login Failed")
 
     return isUser
 
 
-def loadDatabase():
-    print()
+def userRegMenu():
+    username = input("Username:")
+    password = input("Password:")
+
+    userData = open('loginData.txt', 'w')
+    print(f"{username},{password}", file=userData)
+    print("User have been register! Please login!")
+
+
+def manageMenu():
+
+    while True:
+        userInput = checkForNum("1. Display Aircraft"
+                                "\n2. Add Aircraft"
+                                "\n3. Remove Aircraft"
+                                "\n4. Update Aircraft"
+                                "\n5. Quit"
+                                "\n>>")
+
+        if userInput == 1:
+            displayAircraft()
+
+        elif userInput == 2:
+            addAircraft()
+
+        elif userInput == 3:
+            removeAircraft()
+
+        elif userInput == 4:
+            updateAircraft()
+
+        elif userInput == 5:
+            break
+
+        else:
+            print("Not an option!")
+
+
+def displayAircraft():
+    mycursor.execute("SELECT * FROM aircraft")
+
+    result = mycursor.fetchall()
+
+    for row in result:
+        print(row)
+
+
+def addAircraft():
+    sqlFormula = "INSERT INTO aircraft VALUES (%s, %s, %s, %s)"
+
+    reg = input("Registration No.:")
+    maker = input("Maker:")
+    series = input("Series:")
+    price = input("Price:")
+
+    aircraft = (reg, maker, series, price)
+
+    mycursor.execute(sqlFormula, aircraft)
+
+    mydb.commit()
+
+
+def removeAircraft():
+
+
+def updateAircraft():
 
 
 def main():
-    initDatabase()
 
     while True:
         mainMenuInput = mainMenu()
 
         if mainMenuInput == 1:
-            isUser = loginMenu()
+            isUser = loginOption()
 
-            if isUser == True:
-                loadDatabase()
+            if isUser is True:
+                manageMenu()
 
-        if mainMenuInput == 2:
-            print()
+        elif mainMenuInput == 2:
+            displayAircraft()
 
+        else:
+            print("Not an option!")
 
-main()
+# main()
